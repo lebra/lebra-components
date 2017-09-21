c566941bdb9202e8c1cbc87857a71ef78073f762
BoyuZhou <386607913@qq.com>
2017-09-21 10:34:05 +0800

    修改info-lighter颜色

---
+++ src/i18n.js
@@ -0,0 +1,14 @@
+module.exports = {
+    'zh-cn': {
+        'Input': {
+            'ok': '确定',
+            'cancel': '取消',
+            'isee': '知道了'
+        }
+    },
+    'en-us': {
+        'ok': 'ok',
+        'cancel': 'cancel',
+        'isee': 'ok'
+    }
+}
